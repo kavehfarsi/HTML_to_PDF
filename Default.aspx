520c8e1b65475bf103c88cee349dcc95bdbdd04e
Kaveh Farsi Nezhad <98241768+kavehfarsi@users.noreply.github.com>
2022-03-30 06:46:08 +0430

    Initial files uploaded

---
+++ Default.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" EnableEventValidation="false" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="text-align:center;">
+            <h1>Creating PDF file in ASP.NET</h1>
+            <table border="2" width="150" style="text-align:center;margin:0 auto;font-size:16px">
+                <tr><th>Country</th><th>continent</th></tr>
+                <tr><td>Japan</td><td>asia</td></tr>
+                <tr><td>Italy</td><td>europe</td></tr>
+                <tr><td>India</td><td>asia</td></tr>
+                <tr><td>Brazil</td><td>south america</td></tr>
+        
+            </table>
+            <asp:Button ID="btnPdf" runat="server" Text="Create PDF file" style="margin-top:10px;background-color:#0b7545;color:#fff;font-size:20px" OnClick="btnPdf_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using iTextSharp.text.html.simpleparser;
+using iTextSharp.text.pdf;
+using System.IO;
+using iTextSharp.text;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnPdf_Click(object sender, EventArgs e)
+    {
+        Response.ContentType = "application/pdf";
+        Response.AddHeader("content-disposition", "attachment;filename=file.pdf");
+        Response.Cache.SetCacheability(HttpCacheability.NoCache);
+        StringWriter sw = new StringWriter();
+        HtmlTextWriter hw = new HtmlTextWriter(sw);
+        this.Page.RenderControl(hw);
+        StringReader sr = new StringReader(sw.ToString());
+        Document pdfDoc = new Document(PageSize.A4, 10f, 10f, 100f, 0f);
+        HTMLWorker htmlparser = new HTMLWorker(pdfDoc);
+        PdfWriter.GetInstance(pdfDoc, Response.OutputStream);
+        pdfDoc.Open();
+        htmlparser.Parse(sr);
+        pdfDoc.Close();
+        Response.Write(pdfDoc);
+        Response.End();
+    }
+}
